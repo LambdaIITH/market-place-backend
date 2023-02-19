539030f621d0ddd96f0a67cc2011a7a502118388
MikiPAUL <mikipaul46@gmail.com>
2023-02-19 13:32:43 +0530

    db/init.db bug fixed

--- db/init.sql
+++ db/init.sql
@@ -1,7 +1,7 @@
-CREATE TABLE users (
+CREATE TABLE users(
   name VARCHAR(255) NOT NULL,
   email VARCHAR(255) PRIMARY KEY,
-  phone_number NUMERIC(10) NOT NULL,
+  phone_number NUMERIC(10) NOT NULL
 );
 
 CREATE TABLE items (
@@ -13,7 +13,7 @@ CREATE TABLE items (
   date_of_posting TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   date_of_sale TIMESTAMP,
   status BOOLEAN NOT NULL DEFAULT FALSE, -- false = not sold, true = sold
-  FOREIGN KEY (seller_email) REFERENCES users(email),
+  FOREIGN KEY (seller_email) REFERENCES users(email)
 );
 
 CREATE TABLE bids (
@@ -23,12 +23,11 @@ CREATE TABLE bids (
   bid_amount NUMERIC(10,2) NOT NULL,
   date_of_bid TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   FOREIGN KEY (item_id) REFERENCES items(id),
-  FOREIGN KEY (bidder_email) REFERENCES users(email),
-  PRIMARY KEY (item_id, bidder_email),
+  FOREIGN KEY (bidder_email) REFERENCES users(email)
 );
 
 CREATE TABLE sales (
   id SERIAL PRIMARY KEY,
   bid_id INTEGER NOT NULL,
-  FOREIGN KEY (bid_id) REFERENCES bids(id),
+  FOREIGN KEY (bid_id) REFERENCES bids(id)
 );
