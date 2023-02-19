7358bc963b4d16d211080ed5e78e340ce8238181
kdebeast17 <rutvkocheta@gmail.com>
2023-02-19 12:10:33 +0530

    added auth decorator

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import json
 from typing import Union
-from datetime import datetime
 from fastapi import FastAPI, Request
 from authlib.integrations.starlette_client import OAuth, OAuthError
 from dotenv import load_dotenv
@@ -11,14 +9,15 @@ from pydantic import BaseModel
 from starlette.middleware.sessions import SessionMiddleware
 from starlette.responses import RedirectResponse, HTMLResponse
 import aiosql
-
-# load environment variables
+from functools import wraps
 load_dotenv(".env")
 DATABASE = os.getenv("DATABASE")
 POSTGRES_USER = os.getenv("POSTGRES_USER")
 POSTGRES_PASS = os.getenv("POSTGRES_PASS")
 POSTGRES_HOST = os.getenv("POSTGRES_HOST")
 POSTGRES_PORT = os.getenv("POSTGRES_PORT")
+SECRET_KEY = os.getenv("SECRET_KEY")
+
 conn = psycopg2.connect(
     database=DATABASE,
     user=POSTGRES_USER,
@@ -27,12 +26,22 @@ conn = psycopg2.connect(
     port=POSTGRES_PORT,
     cursor_factory=RealDictCursor,
 )
-SECRET_KEY = os.getenv("SECRET_KEY")
-print("Opened database successfully!")
+#print("Opened database successfully!")
 queries = aiosql.from_path("db/init.sql", "psycopg2")
-print(queries)
-queries.create_schema(conn)
-conn.commit()
+
+#wrapper function to check if the user is the seller
+def user_check(func):
+    @wraps(func)
+    async def innerfunction(*args, **kwargs):
+        user = kwargs['request'].session.get('user')
+        seller_email = queries.get_seller_email(conn, item_id=kwargs['bid'].item_id)
+        if user is not None:
+            user_email = user['email']
+            if user_email == seller_email[0]['seller_email']:
+                return await func(*args, **kwargs)
+        return {"error": "You are not the seller"}
+    return innerfunction
+        
 
 app = FastAPI()
 app.add_middleware(SessionMiddleware, secret_key=SECRET_KEY)
@@ -79,7 +88,7 @@ async def new_item(item: Item):
     
 #route to insert bids of a user on a particular item
 @app.post("/new_bid")
-async def new_bid( bid: Bid):
+async def new_bid(bid: Bid):
     queries.create_bid(conn, item_id=bid.item_id, bidder_email=bid.bidder_email, bid_amount=bid.bid_amount)
     conn.commit()
 
@@ -88,9 +97,11 @@ async def new_bid( bid: Bid):
 async def get_bids(item_id: int):
     return queries.get_bids(conn, item_id=item_id)
 
-#route to accept a bid for an item
+#route to accept a bid for an item, only the seller can accept the bid(uses wrapper function)
 @app.post("/accept_bid")
-async def accept_bid(bid: Bid):
+@user_check
+async def accept_bid(request:Request,bid: Bid):
+    # queries.update_bid(conn, item_id=bid.item_id, bidder_email=bid.bidder_email, bid_amount=bid.bid_amount)
     queries.accept_bid(conn, item_id=bid.item_id)
     conn.commit()
 
